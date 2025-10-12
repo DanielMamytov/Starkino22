57ebe75ff168c0cea57d0a2a2f287e407dfc2cff
DanielMamytov <mamytovdaniel.08.02@gmail.com>
2025-10-12 19:39:53 +0600

    Add navigation from dashboard buttons to feature fragments

--- app/src/main/java/co/nisari/katisnar/presentation/ui/main/DashBoardFragment.kt
+++ app/src/main/java/co/nisari/katisnar/presentation/ui/main/DashBoardFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import co.nisari.katisnar.R
 
 class DashBoardFragment : Fragment() {
@@ -20,5 +22,29 @@ class DashBoardFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_dash_board, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<FrameLayout>(R.id.btn_star_location).setOnClickListener {
+            findNavController().navigate(R.id.action_dashBoardFragment_to_starLocationFragment)
+        }
+
+        view.findViewById<FrameLayout>(R.id.btn_star_routes).setOnClickListener {
+            findNavController().navigate(R.id.action_dashBoardFragment_to_starRouteFragment)
+        }
+
+        view.findViewById<FrameLayout>(R.id.btn_star_notes).setOnClickListener {
+            findNavController().navigate(R.id.action_dashBoardFragment_to_starNoteFragment)
+        }
+
+        view.findViewById<FrameLayout>(R.id.btn_starkino_articles).setOnClickListener {
+            findNavController().navigate(R.id.action_dashBoardFragment_to_starArticleFragment)
+        }
+
+        view.findViewById<FrameLayout>(R.id.btn_setting).setOnClickListener {
+            findNavController().navigate(R.id.action_dashBoardFragment_to_settingsFragment)
+        }
     }
 }
